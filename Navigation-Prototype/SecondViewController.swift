9e43527aee9898eb2b9d3796c15ffc9647389cb6
Tim <tyusipov@avito.ru>
2016-01-21 13:38:52 +0300

    Initial Commit

---
+++ Navigation-Prototype/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Navigation-Prototype
+//
+//  Created by Тимур Юсипов on 21/01/16.
+//  Copyright © 2016 Тимур Юсипов. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

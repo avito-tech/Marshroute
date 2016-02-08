8c63f2d3783f9df0a2158e825ba3aa5fc06e641c
Tim <tyusipov@avito.ru>
2016-02-08 12:23:02 +0300

    PR fixes

--- Navigation-Prototype/Transitions/Transition Animations/PopoverAnimations/PopoverTranstionsAnimator.swift
+++ Navigation-Prototype/Transitions/Transition Animations/PopoverAnimations/PopoverTranstionsAnimator.swift
@@ -22,7 +22,7 @@ extension PopoverTranstionsAnimator: TransitionsAnimator {
         }
         
         // только так можно отключить кнопки на navigation bar'е из которого, вызвали поповер
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.3 * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.5 * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), {
             context.popoverController.passthroughViews = nil
         })
     }

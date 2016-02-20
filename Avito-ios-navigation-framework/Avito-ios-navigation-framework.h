4241abca15247820e67b905eb3e8bf214d4a3e7f
Tim <tyusipov@avito.ru>
2016-02-20 10:29:10 +0300

    created a framework target, to deploy against iOS 7.0

---
+++ Avito-ios-navigation-framework/Avito-ios-navigation-framework.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Avito-ios-navigation-framework.
+FOUNDATION_EXPORT double Avito_ios_navigation_frameworkVersionNumber;
+
+//! Project version string for Avito-ios-navigation-framework.
+FOUNDATION_EXPORT const unsigned char Avito_ios_navigation_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Avito_ios_navigation_framework/PublicHeader.h>
+
+

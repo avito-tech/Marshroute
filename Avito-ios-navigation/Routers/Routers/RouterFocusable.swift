5f5aa23cca6259304f8631ec28d1ecad73cfce5d
Tim <tyusipov@avito.ru>
2016-03-04 12:55:50 +0300

    AI-1778: Add completion block to `RouterFocusable` protocol

--- Avito-ios-navigation/Routers/Routers/RouterFocusable.swift
+++ Avito-ios-navigation/Routers/Routers/RouterFocusable.swift
@@ -1,11 +1,16 @@
+import UIKit
+
 /// Методы, чтобы вернуться на экран текущего модуля
 public protocol RouterFocusable: class {
-    func focusOnCurrentModule()
+    func focusOnCurrentModule(completion completion: (() -> Void)?)
 }
 
 // MARK: - RouterFocusable Default Impl
 extension RouterFocusable where Self: RouterTransitionable, Self: RouterIdentifiable {
-    public func focusOnCurrentModule() {
+    public func focusOnCurrentModule(completion completion: (() -> Void)?) {
+        CATransaction.begin()
+        CATransaction.setCompletionBlock(completion) // дожидаемся анимации возвращения на текущий модуль
         transitionsHandlerBox?.unbox().undoTransitionsAfter(transitionId: transitionId)
+        CATransaction.commit()
     }
 }

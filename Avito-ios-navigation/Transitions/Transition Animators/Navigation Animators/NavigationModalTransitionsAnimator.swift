7280e6023c57a6d61b83ccfa3ef0a3e7bd469bd1
Tim <tyusipov@avito.ru>
2016-03-07 10:52:03 +0300

    AI-1778: Add convenient Modal Transitions Animator

---
+++ Avito-ios-navigation/Transitions/Transition Animators/Navigation Animators/NavigationModalTransitionsAnimator.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+public enum ModalTransitionStyle: Int {
+    case CoverVertical
+    case FlipHorizontal
+    case CrossDissolve
+    case PartialCurl
+}
+
+public enum ModalPresentationStyle : Int {
+    case FullScreen
+    case PageSheet
+    case FormSheet
+    case CurrentContext
+    case Custom
+    @available(iOS 8.0, *)
+    case OverFullScreen
+    @available(iOS 8.0, *)
+    case OverCurrentContext
+    @available(iOS 8.0, *)
+    case Popover
+    case None
+}
+
+public final class NavigationModalTransitionsAnimator: NavigationTransitionsAnimator {
+    public let modalTransitionStyle: ModalTransitionStyle
+    public let modalPresentationStyle: ModalPresentationStyle
+    
+    public init(
+        modalTransitionStyle: ModalTransitionStyle?,
+        modalPresentationStyle: ModalPresentationStyle?)
+    {
+        self.modalTransitionStyle = modalTransitionStyle ?? .CoverVertical
+        self.modalPresentationStyle = modalPresentationStyle ?? .FullScreen
+
+        super.init()
+    }
+    
+    override public func animatePerformingTransition(animationContext context: NavigationAnimationContext)
+    {
+        switch context.transitionStyle {
+        case .Modal:
+            
+            if let modalTransitionStyle = UIModalTransitionStyle(rawValue: modalTransitionStyle.rawValue) {
+                context.targetViewController.modalTransitionStyle = modalTransitionStyle
+            }
+            
+            if let modalPresentationStyle = UIModalPresentationStyle(rawValue: modalPresentationStyle.rawValue) {
+                context.targetViewController.modalPresentationStyle = modalPresentationStyle
+            }
+            
+        default:
+            break
+        }
+        
+        super.animatePerformingTransition(animationContext: context)
+    }
+}

20af2ee5e38729bf4e2ec2e6d93a25e883fd4640
Tim <tyusipov@avito.ru>
2016-03-29 19:13:50 +0300

    AI-2184: Convenince methods on `TransitionAnimationContext` for enabling and disabling animations

--- Avito-ios-navigation/Transitions/Transitions Handling/Transitions Coordinator/TransitionsCoordinatorDelegate.swift
+++ Avito-ios-navigation/Transitions/Transitions Handling/Transitions Coordinator/TransitionsCoordinatorDelegate.swift
@@ -3,6 +3,24 @@ import Foundation
 public enum TransitionAnimationContext {
     case Navigation(animator: NavigationTransitionsAnimator)
     case Popover(animator: PopoverTransitionsAnimator)
+    
+    public func becomeAnimated() {
+        switch self {
+        case .Navigation(let animator):
+            animator.becomeAnimated()
+        case .Popover(let animator):
+            animator.becomeAnimated()
+        }
+    }
+    
+    public func becomeNotAnimated() {
+        switch self {
+        case .Navigation(let animator):
+            animator.becomeNotAnimated()
+        case .Popover(let animator):
+            animator.becomeNotAnimated()
+        }
+    }
 }
 
 public protocol TransitionsCoordinatorDelegate: class {
@@ -14,7 +32,7 @@ public protocol TransitionsCoordinatorDelegate: class {
     func transitionsCoordinator(
         coordinator coordinator: TransitionsCoordinator,
         willLaunchPerfromingAnimation context: TransitionAnimationContext,
-        ofTransitionWithId: TransitionId)
+        ofTransitionWithId transitionId: TransitionId)
     
     func transitionsCoordinator(
         coordinator coordinator: TransitionsCoordinator,

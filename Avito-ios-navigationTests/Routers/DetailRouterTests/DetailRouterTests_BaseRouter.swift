325c90aa8ff3f783ed4d232bf2c8cb086b138db4
Tim <tyusipov@avito.ru>
2016-04-24 00:17:15 +0300

    AI-2346: Add `DetailRouter` tests

---
+++ Avito-ios-navigationTests/Routers/DetailRouterTests/DetailRouterTests_BaseRouter.swift
@@ -0,0 +1,137 @@
+import XCTest
+
+final class DetailRouterTests_BaseRouter: XCTestCase
+{
+    var transitionIdGenerator: TransitionIdGenerator!
+    var transitionsCoordinator: TransitionsCoordinator!
+    
+    var detailAnimatingTransitionsHandlerSpy: AnimatingTransitionsHandlerSpy!
+    var targetViewController: UIViewController!
+    var nextModuleRouterSeed: RouterSeed!
+    
+    var router: BaseRouter!
+    
+    override func setUp() {
+        super.setUp()
+        
+        transitionIdGenerator = TransitionIdGeneratorImpl()
+        
+        transitionsCoordinator = TransitionsCoordinatorImpl(
+            stackClientProvider: TransitionContextsStackClientProviderImpl()
+        )
+        
+        detailAnimatingTransitionsHandlerSpy = AnimatingTransitionsHandlerSpy(
+            transitionsCoordinator: transitionsCoordinator
+        )
+        
+        targetViewController = UIViewController()
+        
+        router = BaseRouter(
+            routerSeed: RouterSeed(
+                transitionsHandlerBox: .init(
+                    animatingTransitionsHandler: detailAnimatingTransitionsHandlerSpy
+                ),
+                transitionId: transitionIdGenerator.generateNewTransitionId(),
+                presentingTransitionsHandler: nil,
+                transitionsCoordinator: transitionsCoordinator,
+                transitionIdGenerator: transitionIdGenerator,
+                controllersProvider: RouterControllersProviderImpl()
+            )
+        )
+    }
+    
+    func testThatRouterCallsItsTransitionHandlerOn_SetViewControllerDerivedFrom_WithCorrectResettingContext() {
+        // When
+        router.setViewControllerDerivedFrom { (routerSeed) -> UIViewController in
+            nextModuleRouterSeed = routerSeed
+            return targetViewController
+        }
+        
+        // Then
+        XCTAssert(detailAnimatingTransitionsHandlerSpy.resetWithTransitionCalled)
+        
+        let resettingContext = detailAnimatingTransitionsHandlerSpy.resetWithTransitionContextParameter
+        XCTAssertEqual(resettingContext.transitionId, nextModuleRouterSeed.transitionId)
+        XCTAssertEqual(resettingContext.transitionId, nextModuleRouterSeed.transitionId)
+        XCTAssert(resettingContext.targetViewController === targetViewController)
+        XCTAssert(resettingContext.targetTransitionsHandlerBox.unbox() === detailAnimatingTransitionsHandlerSpy)
+        XCTAssertNil(resettingContext.storableParameters)
+        if case .ResettingNavigationRoot(_) = resettingContext.resettingAnimationLaunchingContextBox {} else { XCTFail() }
+    }
+    
+    func testThatRouterCallsItsTransitionHandlerOn_SetViewControllerDerivedFrom_WithCorrectResettingContext_IfCustomAnimator() {
+        // Given
+        let resetNavigationTransitionsAnimator = ResetNavigationTransitionsAnimator()
+        
+        router = BaseRouter(
+            routerSeed: RouterSeed(
+                transitionsHandlerBox: .init(
+                    animatingTransitionsHandler: detailAnimatingTransitionsHandlerSpy
+                ),
+                transitionId: transitionIdGenerator.generateNewTransitionId(),
+                presentingTransitionsHandler: nil,
+                transitionsCoordinator: transitionsCoordinator,
+                transitionIdGenerator: transitionIdGenerator,
+                controllersProvider: RouterControllersProviderImpl()
+            )
+        )
+        
+        // When
+        router.setViewControllerDerivedFrom( { (routerSeed) -> UIViewController in
+            nextModuleRouterSeed = routerSeed
+            return targetViewController
+            }, animator: resetNavigationTransitionsAnimator
+        )
+        
+        // Then
+        XCTAssert(detailAnimatingTransitionsHandlerSpy.resetWithTransitionCalled)
+        
+        let resettingContext = detailAnimatingTransitionsHandlerSpy.resetWithTransitionContextParameter
+        XCTAssertEqual(resettingContext.transitionId, nextModuleRouterSeed.transitionId)
+        XCTAssertEqual(resettingContext.transitionId, nextModuleRouterSeed.transitionId)
+        XCTAssert(resettingContext.targetViewController === targetViewController)
+        XCTAssert(resettingContext.targetTransitionsHandlerBox.unbox() === detailAnimatingTransitionsHandlerSpy)
+        XCTAssertNil(resettingContext.storableParameters)
+        if case .ResettingNavigationRoot(let launchingContext) = resettingContext.resettingAnimationLaunchingContextBox {
+            XCTAssert(launchingContext.animator === resetNavigationTransitionsAnimator)
+        } else { XCTFail() }
+    }
+    
+    func testThatRouterCallsItsTransitionHandlerOn_PushViewControllerDerivedFrom_WithCorrectPresentationContext() {
+        // When
+        router.pushViewControllerDerivedFrom { (routerSeed) -> UIViewController in
+            nextModuleRouterSeed = routerSeed
+            return targetViewController
+        }
+        
+        let presentationContext = detailAnimatingTransitionsHandlerSpy.perFormTransitionContextParameter
+        XCTAssertEqual(presentationContext.transitionId, nextModuleRouterSeed.transitionId)
+        XCTAssertEqual(presentationContext.transitionId, nextModuleRouterSeed.transitionId)
+        XCTAssert(presentationContext.targetViewController === targetViewController)
+        if case .PendingAnimating = presentationContext.targetTransitionsHandlerBox {} else { XCTFail() }
+        XCTAssertNil(presentationContext.storableParameters)
+        if case .Push(_) = presentationContext.presentationAnimationLaunchingContextBox {} else { XCTFail() }
+    }
+    
+    func testThatRouterCallsItsTransitionHandlerOn_PushViewControllerDerivedFrom_WithCorrectPresentationContext_IfCustomAnimator() {
+        // Given
+        let navigationTransitionsAnimator = NavigationTransitionsAnimator()
+        
+        // When
+        router.pushViewControllerDerivedFrom( { (routerSeed) -> UIViewController in
+            nextModuleRouterSeed = routerSeed
+            return targetViewController
+            }, animator: navigationTransitionsAnimator
+        )
+        
+        let presentationContext = detailAnimatingTransitionsHandlerSpy.perFormTransitionContextParameter
+        XCTAssertEqual(presentationContext.transitionId, nextModuleRouterSeed.transitionId)
+        XCTAssertEqual(presentationContext.transitionId, nextModuleRouterSeed.transitionId)
+        XCTAssert(presentationContext.targetViewController === targetViewController)
+        if case .PendingAnimating = presentationContext.targetTransitionsHandlerBox {} else { XCTFail() }
+        XCTAssertNil(presentationContext.storableParameters)
+        if case .Push(let launchingContext) = presentationContext.presentationAnimationLaunchingContextBox {
+            XCTAssert(launchingContext.animator === navigationTransitionsAnimator)
+        } else { XCTFail() }
+    }
+}

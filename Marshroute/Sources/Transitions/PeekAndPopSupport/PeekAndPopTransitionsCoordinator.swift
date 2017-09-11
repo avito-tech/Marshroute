cff34e2d95a08d1362eed7f619d5dbc21fc66694
Timur Yusipov <tyusipov@avito.ru>
2017-09-11 18:54:35 +0300

    AI-7116: Document `PeekAndPopStateObservable` and `PeekAndPopUtility` protocols

--- Marshroute/Sources/Transitions/PeekAndPopSupport/PeekAndPopTransitionsCoordinator.swift
+++ Marshroute/Sources/Transitions/PeekAndPopSupport/PeekAndPopTransitionsCoordinator.swift
@@ -1,5 +1,8 @@
 import UIKit
 
+/// This protocol is used by `Marshroute` transitions coordinating system 
+/// to allow intercepting transitions during active `peek` sessions.
+/// More on this in `PeekAndPopUtility`
 public protocol PeekAndPopTransitionsCoordinator: class {
     func coordinatePeekIfNeededFor(
         viewController: UIViewController,

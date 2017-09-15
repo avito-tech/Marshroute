f599af0d7fc08ce4d77797f08e35632689780ec3
Timur Yusipov <tyusipov@avito.ru>
2017-09-16 00:49:39 +0300

    AI-7195: Rename project and targets

---
+++ Example/Pods/Target Support Files/Pods-MarshrouteExample/Pods-MarshrouteExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MarshrouteExample : NSObject
+@end
+@implementation PodsDummy_Pods_MarshrouteExample
+@end
--- Example/Pods/Target Support Files/Pods-MarshrouteExample/Pods-MarshrouteExample-umbrella.h
+++ Example/Pods/Target Support Files/Pods-MarshrouteExample/Pods-MarshrouteExample-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_NavigationDemoVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_NavigationDemoVersionString[];
+FOUNDATION_EXPORT double Pods_MarshrouteExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MarshrouteExampleVersionString[];
 

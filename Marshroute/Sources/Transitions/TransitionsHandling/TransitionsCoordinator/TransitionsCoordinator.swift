b70ec1910e73e6f3190f78bb27625d41ae9f77d4
Timur Yusipov <tyusipov@avito.ru>
2017-09-16 01:31:47 +0300

    AI-7195: Remove outdated comments

--- Marshroute/Sources/Transitions/TransitionsHandling/TransitionsCoordinator/TransitionsCoordinator.swift
+++ Marshroute/Sources/Transitions/TransitionsHandling/TransitionsCoordinator/TransitionsCoordinator.swift
@@ -169,7 +169,7 @@ extension TransitionsCoordinator where
         forAnimatingTransitionsHandler transitionsHandler: AnimatingTransitionsHandler)
     {
         // скрываем модальные окна и поповеры, показанные внутри модальных окон и поповеров текущего обработчика
-        coordinateUndoingChainedTransitionsIfNeeded(forTransitionsHandler: transitionsHandler)
+        coordinateUndoingChainedTransitionsWithoutAnimations(forTransitionsHandler: transitionsHandler)
         
         guard let stackClient = stackClientProvider.stackClient(forTransitionsHandler: transitionsHandler)
             else { return }
@@ -192,7 +192,7 @@ extension TransitionsCoordinator where
         forAnimatingTransitionsHandler transitionsHandler: AnimatingTransitionsHandler)
     {
         // скрываем модальные окна и поповеры, показанные внутри модальных окон и поповеров текущего обработчика
-        coordinateUndoingChainedTransitionsIfNeeded(forTransitionsHandler: transitionsHandler)
+        coordinateUndoingChainedTransitionsWithoutAnimations(forTransitionsHandler: transitionsHandler)
         
         guard let stackClient = stackClientProvider.stackClient(forTransitionsHandler: transitionsHandler)
             else { return }
@@ -226,7 +226,7 @@ extension TransitionsCoordinator where
         var context = context
         
         // скрываем модальные окна и поповеры, показанные внутри модальных окон и поповеров текущего обработчика
-        coordinateUndoingChainedTransitionsIfNeeded(forTransitionsHandler: animatingTransitionsHandler)
+        coordinateUndoingChainedTransitionsWithoutAnimations(forTransitionsHandler: animatingTransitionsHandler)
         
         // ищем существующую историю переходов или создаем новую
         let stackClient = stackClientProvider.stackClient(forTransitionsHandler: animatingTransitionsHandler)
@@ -338,33 +338,6 @@ private extension TransitionsCoordinator where
             includingTransitionWithId: includingTransitionWithId,
             forTransitionsHandler: animatingTransitionsHandler
         )
-    }
-    
-    func coordinateUndoingChainedTransitionsIfNeeded(forTransitionsHandler transitionsHandler: TransitionsHandler)
-    {
-        // по-хорошему нужно убрать все модальные окна и поповеры дочерних обработчиков переходов.
-        // но обнаружились следующие особенности UIKit'а
-        //
-        // 1. iOS 8, 9:     если скрывать последовательность из поповеров,
-        //                      то UIKit падает при анимировании больших (> 3) последовательностей
-        //                  если не скрывать последовательности из поповеров, а скрывать только нижний,
-        //                      то UIKit отрабатывает правильно
-        // 2. iOS 7:        если скрывать последовательность из поповеров,
-        //                      то UIKit отрабатывает правильно
-        //                  если не скрывать последовательности из поповеров, а скрывать только нижний,
-        //                      то UIKit падает, потому что ```popover dealloc reached while popover is visible```
-        // 3. iOS 7, 8, 9:  если скрывать последовательность из модальных окон,
-        //                      то UIKit не падает, но просто не выполняет сокрытие примерно на середине последовательности
-        //
-        // в итоге договорились  не убирать дочерние модальные окна и поповеры,
-        // а на iOS 7 не использовать поповеры вообще или использовать аккуратно:
-        //
-        // а) на iOS 7 не показывать поповер в поповере
-        // б) на iOS 7 не показывать поповеры внутри модальных окон
-        // в) игнорировать пункты а) и б), но не вызывать сокрытие целой цепочки модальных окон и поповеров
-    
-        // в итоге только очищаем историю, но не выполняем анимаций
-        coordinateUndoingChainedTransitionsWithoutAnimations(forTransitionsHandler: transitionsHandler)
     }
     
     func coordinateUndoingChainedTransitionsWithoutAnimations(forTransitionsHandler transitionsHandler: TransitionsHandler)
@@ -517,7 +490,7 @@ private extension TransitionsCoordinator where
         withStackClient stackClient: TransitionContextsStackClient)
     {
         // скрываем модальные окна и поповеры, показанные внутри модальных окон и поповеров текущего обработчика
-        coordinateUndoingChainedTransitionsIfNeeded(forTransitionsHandler: animatingTransitionsHandler)
+        coordinateUndoingChainedTransitionsWithoutAnimations(forTransitionsHandler: animatingTransitionsHandler)
         
         // вызываем анимации сокрытия модальных окон и поповеров
         if let chainedTransition = chainedTransition {

04d31dfaf498f8efcdf8d75c18cdaee50b575c4d
Timur Yusipov <tyusipov@avito.ru>
2017-09-16 13:45:15 +0300

    AI-7195: Slice a big file into several smaller. Group `peek and pop` support files into folders

--- Marshroute/Sources/Transitions/PeekAndPopSupport/PeekAndPopData/StrongPeekAndPopData.swift
+++ Marshroute/Sources/Transitions/PeekAndPopSupport/PeekAndPopData/StrongPeekAndPopData.swift
@@ -1,52 +1,6 @@
 import UIKit
 
-protocol PeekAndPopData {
-    var peekViewController: UIViewController? { get }
-    var sourceViewController: UIViewController? { get }
-    
-    var previewingContext: UIViewControllerPreviewing? { get }
-    var peekLocation: CGPoint { get }
-    
-    var popAction: (() -> ()) { get }
-}
-
-protocol PeekAndPopDataHolder {
-    var peekAndPopData: PeekAndPopData { get }
-}
-
-extension PeekAndPopData where Self: PeekAndPopDataHolder {
-    var peekViewController: UIViewController? {
-        return peekAndPopData.peekViewController
-    }
-    
-    var sourceViewController: UIViewController? {
-        return peekAndPopData.sourceViewController
-    }
-    
-    var previewingContext: UIViewControllerPreviewing? {
-        return peekAndPopData.previewingContext   
-    }
-    
-    var peekLocation: CGPoint {
-        return peekAndPopData.peekLocation
-    }
-    
-    var popAction: (() -> ()) {
-        return peekAndPopData.popAction
-    }
-}
-
-struct WeakPeekAndPopData: PeekAndPopData {
-    weak var peekViewController: UIViewController?
-    weak var sourceViewController: UIViewController?
-   
-    weak var previewingContext: UIViewControllerPreviewing?
-    let peekLocation: CGPoint
-    
-    let popAction: (() -> ())
-}
-
-struct StrongPeekAndPopData: PeekAndPopData, PeekAndPopDataHolder {
+final class StrongPeekAndPopData: PeekAndPopData, PeekAndPopDataHolder {
     // MARK: - properties
     let strongPeekViewController: UIViewController
     let strongSourceViewController: UIViewController
@@ -87,4 +41,3 @@ struct StrongPeekAndPopData: PeekAndPopData, PeekAndPopDataHolder {
         return toWeakPeekAndPopData()
     }
 }
-

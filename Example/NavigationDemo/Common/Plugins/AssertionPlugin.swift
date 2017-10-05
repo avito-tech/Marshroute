8e5fce9e615a54adfc41d1bc752ffb2206f4e421
Vladimir Ignatov <vyuignatov@avito.ru>
2017-10-05 17:44:11 +0300

    AI-7358: Add demo assertion plugin

---
+++ Example/NavigationDemo/Common/Plugins/AssertionPlugin.swift
@@ -0,0 +1,14 @@
+import Marshroute
+
+final class DemoAssertionPlugin: MarshrouteAssertionPlugin {
+    private let demoPrefix = "NavigatonDemo "
+    
+    func assert(
+        _ condition: @autoclosure () -> Bool,
+        _ message: @autoclosure () -> String,
+        file: StaticString,
+        line: UInt) {
+        let demoMessage = demoPrefix + message()
+        Swift.assert(condition(), demoMessage, file: file, line: line)
+    }
+}
